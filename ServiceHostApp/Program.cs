5e02f2c8cb7553b354423a1abfff60a977a0051f
Jarosław <jaroslawbolejko85@gmail.com>
2025-08-21 13:15:13 +0200

    Move client configuration to app.config
    Coniguration files are proper place to keep conigs

--- ServiceHostApp/Program.cs
+++ ServiceHostApp/Program.cs
@@ -5,29 +5,14 @@ using System.ServiceModel.Description;
 
 namespace ServiceHostApp
 {
-    internal class Program
+    public class Program
     {
         static void Main()
         {
-            var baseAddress = new Uri("http://localhost:8080/CalcService");
-
-            using (var host = new ServiceHost(typeof(CalcService), baseAddress))
+            using (var host = new ServiceHost(typeof(CalcService)))
             {
-                // endpoint: Contract + Binding + Address
-                host.AddServiceEndpoint(typeof(ICalculator), new BasicHttpBinding(), "");
-
-                // metadata (WSDL)
-                var smb = new ServiceMetadataBehavior { HttpGetEnabled = true };
-                host.Description.Behaviors.Add(smb);
-                host.AddServiceEndpoint(
-                    typeof(IMetadataExchange),
-                    MetadataExchangeBindings.CreateMexHttpBinding(),
-                    "mex");
-
                 host.Open();
-                Console.WriteLine("WCF works on " + baseAddress);
-                Console.WriteLine("WSDL: " + baseAddress + "?wsdl");
-                Console.WriteLine("Press Enter to exit...");
+                Console.WriteLine("Host is running. Press ENTER to finish...");
                 Console.ReadLine();
             }
         }
