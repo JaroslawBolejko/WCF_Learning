bc9e64dd807fe828e01a94f67935e3e49ff1c7db
Jarosław <jaroslawbolejko85@gmail.com>
2025-08-21 14:11:21 +0200

    Add WsHttpBinding with no security
    Practice task, to check and understand difference

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -8,14 +8,24 @@ namespace ClientApp
     {
         static void Main(string[] args)
         {
-            ChannelFactory<ICalculator> factory = new ChannelFactory<ICalculator>("BasicHttp_Calc");
-            ICalculator channel = factory.CreateChannel();
+            ChannelFactory<ICalculator> basicFactory = new ChannelFactory<ICalculator>("BasicHttp_Calc");
+            ICalculator basicChannel = basicFactory.CreateChannel();
 
-            Console.WriteLine("10 + 20 = " + channel.Add(10, 20));
-            Console.WriteLine("10 * 20 = " + channel.Multiply(10, 20));
+            Console.WriteLine("BasicHttp_calc");
+            Console.WriteLine("10 + 20 = " + basicChannel.Add(10, 20));
+            Console.WriteLine("10 * 20 = " + basicChannel.Multiply(10, 20));
 
-            ((IClientChannel)channel).Close();
-            factory.Close();
+            ((IClientChannel)basicChannel).Close();
+            basicFactory.Close();
+
+            ChannelFactory<ICalculator> wsFactory = new ChannelFactory<ICalculator>("WsHttp_Calc");
+            ICalculator wsChannel = wsFactory.CreateChannel();
+
+            Console.WriteLine("30 + 20 = " + wsChannel.Add(30, 20));
+            Console.WriteLine("30 * 20 = " + wsChannel.Multiply(30, 20));
+
+            ((IClientChannel)wsChannel).Close();
+            wsFactory.Close();
 
             Console.WriteLine("Press Enter to exit...");
             Console.ReadLine();
